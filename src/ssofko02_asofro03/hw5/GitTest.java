bcf24aaece0f6a11f2ce402b608eaaf62938fdf4
stytSofo <ssofok02@ucy.ac.cy>
2020-04-11 22:10:28 +0300

    Commit

--- src/ssofko02_asofro03/hw5/GitTest.java
+++ src/ssofko02_asofro03/hw5/GitTest.java
@@ -3,6 +3,7 @@ package ssofko02_asofro03.hw5;
 public class GitTest {
 	public static void main(String args[]) {
 		System.out.print("Hi are you");
+		System.out.println("Hi this is the other ou");
 	}
 
 }
