b2e5f68b5d3c3c73f6d262bb29d10ff4eef522e5
stytSofo <ssofok02@ucy.ac.cy>
2020-04-11 22:27:23 +0300

    Std

--- src/ssofko02_asofro03/hw5/GitTest.java
+++ src/ssofko02_asofro03/hw5/GitTest.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.introcs.StdOut;
 public class GitTest {
 	public static void main(String args[]) {
 		StdOut.print();
-		//some code here too
+		//some code here to
 		// im doing work witout notice ahahahah
 
 	}
