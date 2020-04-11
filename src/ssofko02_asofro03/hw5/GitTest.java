7bb1a86085f060143e8f9cfab1b9381b3338136c
stytSofo <ssofok02@ucy.ac.cy>
2020-04-11 22:16:54 +0300

    code

--- src/ssofko02_asofro03/hw5/GitTest.java
+++ src/ssofko02_asofro03/hw5/GitTest.java
@@ -3,13 +3,7 @@ package ssofko02_asofro03.hw5;
 public class GitTest {
 	public static void main(String args[]) {
 
-		System.out.println("Hi are you");
-		
-		System.out.println("THis is also me editing");
-
-		System.out.print("Hi are you");
-		System.out.println("Hi this is the other ou");
-		System.out.print("THis is also me editing");
+		//some code here too
 		// im doing work witout notice ahahahah
 
 	}
