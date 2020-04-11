ad3d405db373c0a73e3289451a77739d5824a2f4
stytSofo <ssofok02@ucy.ac.cy>
2020-04-11 22:26:44 +0300

    Stdib

--- src/ssofko02_asofro03/hw5/GitTest.java
+++ src/ssofko02_asofro03/hw5/GitTest.java
@@ -1,8 +1,10 @@
 package ssofko02_asofro03.hw5;
 
+import edu.princeton.cs.introcs.StdOut;
+
 public class GitTest {
 	public static void main(String args[]) {
-
+		StdOut.print();
 		//some code here too
 		// im doing work witout notice ahahahah
 
