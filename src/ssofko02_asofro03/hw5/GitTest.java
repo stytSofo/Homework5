118367bbfba9584d1facf4355789c06e49722ef3
stytSofo <ssofok02@ucy.ac.cy>
2020-04-11 22:10:28 +0300

    Commmite

--- src/ssofko02_asofro03/hw5/GitTest.java
+++ src/ssofko02_asofro03/hw5/GitTest.java
@@ -3,6 +3,8 @@ package ssofko02_asofro03.hw5;
 public class GitTest {
 	public static void main(String args[]) {
 		System.out.print("Hi are you");
+		System.out.print("THis is also me editing");
+		
 	}
 
 }
