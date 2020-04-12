1f9650898c1387fc282d56739c37bb6142cd11b5
asofro03 <asofro03@ucy.ac.cy>
2020-04-13 02:23:16 +0300

    Constructor of grid

--- src/sofokleous_sofroniou/hw5/Grid.java
+++ src/sofokleous_sofroniou/hw5/Grid.java
@@ -7,115 +7,119 @@ public class Grid {
 	private People[] people;
 	private int population;
 
-
 	public Grid(int N, int infectedPopulation, int population, double probToHaveProtection,
 			double probGiveInfectionWithProtection, double probGiveInfectionWithoutProtection,
-			double probGetInfectionWithProtection, double probGetInfectionWithoutProtection, double probToBeImmune, double probCellToGiveInfection) {
+			double probGetInfectionWithProtection, double probGetInfectionWithoutProtection, double probToBeImmune,
+			double probCellToGiveInfection) {
 
-		this.N=N;
+		this.N = N;
 		this.grid = new Cell[N][N];
-		this.population=population;
-		
-		people= new People[population];
-		
-		for(int i=0; i<infectedPopulation; i++) {
-			Point position=findRandomPosition();
-			people[i]=new People(true, position,  probToHaveProtection,  probGiveInfectionWithProtection,
-					 probGiveInfectionWithoutProtection,  probGetInfectionWithProtection,
-					 probGetInfectionWithoutProtection,  probToBeImmune);
-			grid[(int) position.getX()][(int) position.getY()]= new Cell( true, true, probCellToGiveInfection, 0);
+		InitialiseGrid();
+		this.population = population;
+
+		people = new People[population];
+
+		for (int i = 0; i < infectedPopulation; i++) {
+			Point position = findRandomPosition();
+			people[i] = new People(true, position, probToHaveProtection, probGiveInfectionWithProtection,
+					probGiveInfectionWithoutProtection, probGetInfectionWithProtection,
+					probGetInfectionWithoutProtection, probToBeImmune);
+			grid[(int) position.getX()][(int) position.getY()] = new Cell(true, true, probCellToGiveInfection, 0);
 		}
 		
+		for (int i = infectedPopulation; i < population; i++) {
+			Point position = findRandomPosition();
+			people[i] = new People(false, position, probToHaveProtection, probGiveInfectionWithProtection,
+					probGiveInfectionWithoutProtection, probGetInfectionWithProtection,
+					probGetInfectionWithoutProtection, probToBeImmune);
+			grid[(int) position.getX()][(int) position.getY()] = new Cell(true, false, probCellToGiveInfection, 0);
+		}
 
 	}
-	
+
+	public void InitialiseGrid() {
+		for (int row = 0; row < this.N; row++)
+			for (int col = 0; col < this.N; col++)
+				grid[row][col] = new Cell();
+	}
+
 	public Point findRandomPosition() {
-		int x,y; 
+		int x, y;
 		do {
-			x= (int)((Math.random())*this.N);
-			y= (int)((Math.random())*this.N);
-		}while(grid[x][y].isOccupied());
+			x = (int) ((Math.random()) * this.N);
+			y = (int) ((Math.random()) * this.N);
+		} while (grid[x][y].isOccupied());
 		return new Point(x, y);
 	}
-		
+
 	public void move(int Time) {
-		for(int i =0;i<people.length;i++) {
-			int move_prob=(int)(Math.random()*8);
-			if(move_prob==0 ) {
-				
-				
+		for (int i = 0; i < people.length; i++) {
+			int move_prob = (int) (Math.random() * 8);
+			if (move_prob == 0) {
+
 			}
-				
+
 		}
 	}
-	
-	private boolean canMoveUp(People p) {//pls commit
+
+	private boolean canMoveUp(People p) {// pls commit
 		int X = (int) p.getPosition().getX();
 		int Y = (int) p.getPosition().getY();
-		
-		if(Y+1>N)
+
+		if (Y + 1 > N)
 			return false;
-		if(grid[X][Y+1].isOccupied())
+		if (grid[X][Y + 1].isOccupied())
 			return false;
-		
-		
+
 		return true;
 	}
-	
-	private boolean canMoveDown(People p) {//pls commit
+
+	private boolean canMoveDown(People p) {// pls commit
 		int X = (int) p.getPosition().getX();
 		int Y = (int) p.getPosition().getY();
-		
-		if(Y-1<0)
+
+		if (Y - 1 < 0)
 			return false;
-		if(grid[X][Y-1].isOccupied())
+		if (grid[X][Y - 1].isOccupied())
 			return false;
-		
-		
+
 		return true;
 	}
-	
-	private boolean canMoveRight(People p) {//pls commit
+
+	private boolean canMoveRight(People p) {// pls commit
 		int X = (int) p.getPosition().getX();
 		int Y = (int) p.getPosition().getY();
-		
-		if(X+1>N)
+
+		if (X + 1 > N)
 			return false;
-		if(grid[X+1][Y].isOccupied())
+		if (grid[X + 1][Y].isOccupied())
 			return false;
-		
-		
+
 		return true;
 	}
-	
-	private boolean canMoveLeft(People p) {//pls commit
+
+	private boolean canMoveLeft(People p) {// pls commit
 		int X = (int) p.getPosition().getX();
 		int Y = (int) p.getPosition().getY();
-		
-		if(X-1<0)
+
+		if (X - 1 < 0)
 			return false;
-		if(grid[X-1][Y].isOccupied())
+		if (grid[X - 1][Y].isOccupied())
 			return false;
-		
-		
+
 		return true;
 	}
-	
+
 	private boolean canMoveDiagUpLeft(People p) {
 		int X = (int) p.getPosition().getX();
 		int Y = (int) p.getPosition().getY();
-		
-		if(X-1<0 || Y+1>N)
+
+		if (X - 1 < 0 || Y + 1 > N)
 			return false;
-		if(grid[X-1][Y].isOccupied())
+		if (grid[X - 1][Y].isOccupied())
 			return false;
-		
-		
+
 		return true;
 	}
-	
 
 }
-	
-
-
