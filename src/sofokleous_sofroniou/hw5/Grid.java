711ac89b8fe1a6c5630c482583b8f22f3055a3de
asofro03 <asofro03@ucy.ac.cy>
2020-04-13 00:46:25 +0300

    Grid constuctor

--- src/sofokleous_sofroniou/hw5/Grid.java
+++ src/sofokleous_sofroniou/hw5/Grid.java
@@ -2,9 +2,13 @@ package sofokleous_sofroniou.hw5;
 
 public class Grid {
 	
-	private Cell[] grid;
+	private Cell[][] grid;
 	private int N;
 	private People[] people;
 	private int population;
+	
+	public Grid(int N, int poppulation) {
+		this.grid=new Cell[N][N];
+	}
 	
 }
