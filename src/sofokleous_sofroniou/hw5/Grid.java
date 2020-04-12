0d37a99ab3d71f577b53e09847d19c31f2def5e4
stytSofo <ssofok02@ucy.ac.cy>
2020-04-12 23:58:30 +0300

    Grid drawing

---
+++ src/sofokleous_sofroniou/hw5/Grid.java
@@ -0,0 +1,18 @@
+package sofokleous_sofroniou.hw5;
+
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Grid {
+	
+	public static void DrawFrame(int N) {
+		
+		
+		StdDraw.setCanvasSize(1000, 1000);
+		StdDraw.setXscale(0, N);
+		StdDraw.setYscale(0,N);
+		
+	}
+	
+	
+
+}
