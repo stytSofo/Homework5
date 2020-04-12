8362a524de5fbc03db5ed5d68aeccba329b333eb
asofro03 <asofro03@ucy.ac.cy>
2020-04-13 00:21:02 +0300

    Class Covid: main

---
+++ src/sofokleous_sofroniou/hw5/Covid.java
@@ -0,0 +1,22 @@
+package sofokleous_sofroniou.hw5;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class Covid {
+	
+/*	private int time;
+	private Grid grid;
+	private People[] population;	*/
+	
+	public static void main(String[] args) {
+		
+		StdOut.println("Give population: ");
+		int population = StdIn.readInt();
+		StdOut.println("Give infected: ");
+		int infected = StdIn.readInt();
+		StdOut.println("Give probability of infection: ");
+		double probGiveInfection = StdIn.readDouble();
+	}
+
+}
