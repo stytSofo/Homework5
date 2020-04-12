67169d267060ea74ba517eec36a798eda4c6f3a8
asofro03 <asofro03@ucy.ac.cy>
2020-04-13 01:07:04 +0300

    People Constructor change

--- src/sofokleous_sofroniou/hw5/People.java
+++ src/sofokleous_sofroniou/hw5/People.java
@@ -9,55 +9,62 @@ public class People {
 	private double probGiveInfection;
 	private double probGetInfection;
 	private boolean immune;
-	
-	
-	public People(Point P) {
-		
-		this.infected=false;
-		this.position=P;
-		if(Math.random()>0.8)
-			this.hasProtection=true;
-		
-		if(this.hasProtection) {
-			this.probGiveInfection=0.05;
-			this.probGetInfection=0.1;
-		}
-		else {
-			this.probGiveInfection=0.15;
-			this.probGetInfection=0.2;
-		}
-		if(Math.random()>0.9)
-			immune=true;
-	}
-	
 
-	
+	public People(boolean infected, Point position, double probToHaveProtection, double probGiveInfectionWithProtection,
+			double probGiveInfectionWithoutProtection, double probGetInfectionWithProtection,
+			double probGetInfectionWithoutProtection, double probToBeImmune) {
+
+		if (Math.random() >= probToBeImmune) {
+			this.immune = true;
+			this.infected = false;
+		} else {
+
+			this.immune = false;
+
+			this.infected = infected;
+
+			this.position = position;
+
+			if (Math.random() >= probToHaveProtection) {
+				this.hasProtection = true;
+				this.probGiveInfection = probGiveInfectionWithProtection;
+				this.probGetInfection = probGetInfectionWithProtection;
+			} else {
+				this.hasProtection = false;
+				this.probGiveInfection = probGiveInfectionWithoutProtection;
+				this.probGetInfection = probGetInfectionWithoutProtection;
+			}
+
+		}
+
+	}
+
 	public Point getPosition() {
 		return position;
 	}
-	
+
 	public void Infect() {
-		infected=true;
+		infected = true;
 	}
-	
+
 	public boolean isInfected() {
 		return infected;
 	}
-	
+
 	public boolean hasProtection() {
 		return hasProtection;
 	}
-	
+
 	public double getProbGiveInfection() {
 		return probGiveInfection;
 	}
-	
+
 	public double getProbGetInfection() {
 		return probGetInfection;
 	}
-	
+
 	public boolean isImmune() {
 		return this.immune;
 	}
-	
+
 }
