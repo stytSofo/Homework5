41ee816db7b33e655720049302a3ba0f54ee6968
asofro03 <asofro03@ucy.ac.cy>
2020-04-13 02:52:29 +0300

    Covid main

--- src/sofokleous_sofroniou/hw5/Covid.java
+++ src/sofokleous_sofroniou/hw5/Covid.java
@@ -4,19 +4,50 @@ import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
 public class Covid {
-	
-/*	private int time;
-	private Grid grid;
-	private People[] population;	*/
-	
+
 	public static void main(String[] args) {
-		
+
+		StdOut.println("Give grid length: ");
+		int N = StdIn.readInt();
+
 		StdOut.println("Give population: ");
 		int population = StdIn.readInt();
-		StdOut.println("Give infected: ");
-		int infected = StdIn.readInt();
-		StdOut.println("Give probability of infection: ");
-		double probGiveInfection = StdIn.readDouble();
-	}
+		if (population > N) {
+			StdOut.println("Population can not be bigger than the area of the grid. Exiting Program.");
+			System.exit(0);
+		}
 
+		StdOut.println("Give infected population: ");
+		int infectedPopulation = StdIn.readInt();
+
+		StdOut.println("Give probability of having protection: ");
+		double probToHaveProtection = StdIn.readDouble();
+
+		StdOut.println("Give probability to transmit the infection with protection: ");
+		double probGiveInfectionWithProtection = StdIn.readDouble();
+
+		StdOut.println("Give probability to transmit the infection with no protection: ");
+		double probGiveInfectionWithoutProtection = StdIn.readDouble();
+
+		StdOut.println("Give probability to get infected with protection: ");
+		double probGetInfectionWithProtection = StdIn.readDouble();
+
+		StdOut.println("Give probability to get infected with no protection: ");
+		double probGetInfectionWithoutProtection = StdIn.readDouble();
+
+		StdOut.println("Give probability to be immune: ");
+		double probToBeImmune = StdIn.readDouble();
+
+		StdOut.println("Give probability to get infected from objects: ");
+		double probCellToGiveInfection = StdIn.readDouble();
+
+		Grid grid = new Grid(N, infectedPopulation, population, probToHaveProtection, probGiveInfectionWithProtection,
+				probGiveInfectionWithoutProtection, probGetInfectionWithProtection, probGetInfectionWithoutProtection,
+				probToBeImmune, probCellToGiveInfection);
+
+		for(int i=1; i<=120; i++)
+			grid.move(i);
+				
+	}
+	
 }
