6c4da6914b24d7d885af891d41f10a12145c1203
asofro03 <asofro03@ucy.ac.cy>
2020-04-12 23:58:02 +0300

    Cell class: constructor + setters/getters

---
+++ src/sofokleous_sofroniou/hw5/Cell.java
@@ -0,0 +1,50 @@
+package sofokleous_sofroniou.hw5;
+
+public class Cell {
+
+	private boolean occupied;
+	private boolean infected;
+	private double probGiveInfection;
+	private int lastOccuppied;
+
+	public Cell() {
+		this.occupied = false;
+		this.infected = false;
+		this.probGiveInfection = 0;
+		this.lastOccuppied = 0;
+	}
+	
+	public boolean isOccupied() {
+		return occupied;
+	}
+
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+
+	public boolean isInfected() {
+		return infected;
+	}
+
+	public void setInfected(boolean infected) {
+		this.infected = infected;
+	}
+
+	public double getProbGiveInfection() {
+		return probGiveInfection;
+	}
+
+	public void setProbGiveInfection(double probGiveInfection) {
+		this.probGiveInfection = probGiveInfection;
+	}
+
+	public int getLastOccuppied() {
+		return lastOccuppied;
+	}
+
+	public void setLastOccuppied(int lastOccuppied) {
+		this.lastOccuppied = lastOccuppied;
+	}
+
+	
+}
