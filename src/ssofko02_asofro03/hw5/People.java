ca8e163be673cfe56ba20979c3849584db0f693f
stytSofo <ssofok02@ucy.ac.cy>
2020-04-12 16:33:46 +0300

    Creation of Point and People class

---
+++ src/ssofko02_asofro03/hw5/People.java
@@ -0,0 +1,16 @@
+package ssofko02_asofro03.hw5;
+
+public class People {
+	private boolean infected;
+	private Point position;
+	private boolean hasProtection;
+	private double probInfection;
+	
+	
+	public People() {
+		
+		this.infected=false;
+		
+	}
+
+}
---
+++ src/ssofko02_asofro03/hw5/Point.java
@@ -0,0 +1,48 @@
+package ssofko02_asofro03.hw5;
+
+import edu.princeton.cs.introcs.StdOut;
+
+public class Point {
+
+	private double x;
+	private double y;
+
+	public Point() {
+		x = 0.0;
+		y = 0.0;
+	}
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void translate(double dx, double dy) {
+
+		x += dx;
+		y += dy;
+
+	}
+
+	public boolean same(Point p1) {
+		return this.x == p1.x && this.y == p1.y;
+	}
+
+	public String toString() {
+		return "coordinate x: " + x + " coordinate y: " + y;
+	}
+	
+	public static void toString(Point tab[]) {
+		for(int i=0;i<tab.length;i++)
+			StdOut.println(tab[i].toString());
+		
+		
+	}
+	public double getY() {
+		return y;
+	}
+	public double getX() {
+		return x;
+	}
+ 
+}
