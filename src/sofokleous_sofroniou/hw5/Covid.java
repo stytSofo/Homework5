04e4f859aff119ce83a05f0549681bde74515e05
asofro03 <asofro03@ucy.ac.cy>
2020-04-15 01:39:05 +0300

    checks main delete exception

--- src/sofokleous_sofroniou/hw5/Covid.java
+++ src/sofokleous_sofroniou/hw5/Covid.java
@@ -14,11 +14,19 @@ public class Covid {
 		try {
 			StdOut.println("Give grid length: ");
 			int N = StdIn.readInt();
+			while (N <= 1) {
+				StdOut.println("The length of the grid should be greater than 1");
+				StdOut.println("Give grid length: ");
+				N = StdIn.readInt();
+			}
 
 			StdOut.println("Give population: ");
 			int population = StdIn.readInt();
-			while (population > N * N) {
-				StdOut.println("Population can not be bigger than the area of the grid.");
+			while (population > N * N || population <= 0) {
+				if (population > N * N)
+					StdOut.println("Population can not be bigger than the area of the grid.");
+				if (population <= 0)
+					StdOut.println("Population should be a positive number");
 				StdOut.println("Give population: ");
 				population = StdIn.readInt();
 			}
@@ -26,8 +34,11 @@ public class Covid {
 			StdOut.println("Give infected population: ");
 			int infectedPopulation = StdIn.readInt();
 
-			while (infectedPopulation > population) {
-				StdOut.println("Infected population can not be bigger than the population.");
+			while (infectedPopulation > population || infectedPopulation <= 0) {
+				if (infectedPopulation > population)
+					StdOut.println("Infected population can not be bigger than the population.");
+				if (infectedPopulation <= 0)
+					StdOut.println("Infected population should be grater than 0");
 				StdOut.println("Give infected population: ");
 				infectedPopulation = StdIn.readInt();
 
@@ -53,7 +64,7 @@ public class Covid {
 			StdOut.println("Give probability to transmit the infection with no protection: ");
 			double probGiveInfectionWithoutProtection = StdIn.readDouble();
 			while ((probGiveInfectionWithProtection > probGiveInfectionWithoutProtection)
-					&& ((probGiveInfectionWithoutProtection < 0) || (probGiveInfectionWithoutProtection > 1))) {
+					|| ((probGiveInfectionWithoutProtection < 0) || (probGiveInfectionWithoutProtection > 1))) {
 				if (probGiveInfectionWithProtection > probGiveInfectionWithoutProtection)
 					StdOut.println(
 							"The probability to infect someone with protection should be lower than the probability to infect someone with no protection");
@@ -74,7 +85,7 @@ public class Covid {
 			StdOut.println("Give probability to get infected with no protection: ");
 			double probGetInfectionWithoutProtection = StdIn.readDouble();
 			while ((probGetInfectionWithProtection > probGetInfectionWithoutProtection)
-					&& ((probGetInfectionWithoutProtection < 0) || (probGetInfectionWithoutProtection > 1))) {
+					|| ((probGetInfectionWithoutProtection < 0) || (probGetInfectionWithoutProtection > 1))) {
 				if (probGetInfectionWithProtection > probGetInfectionWithoutProtection)
 					StdOut.println(
 							"The probability to get infected with protection should be lower than the probability to get infected with no protection");
@@ -94,9 +105,9 @@ public class Covid {
 
 			StdOut.println("Give probability to get infected from objects: ");
 			double probCellToGiveInfection = StdIn.readDouble();
-			while ((probGetInfectionWithoutProtection > probCellToGiveInfection)
-					&& ((probCellToGiveInfection < 0) || (probCellToGiveInfection > 1))) {
-				if (probGetInfectionWithoutProtection > probCellToGiveInfection)
+			while ((probGetInfectionWithoutProtection < probCellToGiveInfection)
+					|| ((probCellToGiveInfection < 0) || (probCellToGiveInfection > 1))) {
+				if (probGetInfectionWithoutProtection < probCellToGiveInfection)
 					StdOut.println(
 							"The probability to get infected by an object is lower than the probability of getting infected from a person");
 				if ((probCellToGiveInfection < 0) || (probCellToGiveInfection > 1))
