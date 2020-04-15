fe558063528dae2f9ba0236cbc5dd5127ff2183b
stytSofo <ssofok02@ucy.ac.cy>
2020-04-15 18:46:08 +0300

    Commit for comments on DrawGrid

--- src/sofokleous_sofroniou/hw5/DrawGrid.java
+++ src/sofokleous_sofroniou/hw5/DrawGrid.java
@@ -2,9 +2,22 @@ package sofokleous_sofroniou.hw5;
 import java.awt.Color;
 
 import edu.princeton.cs.introcs.StdDraw;
-
+/**
+ * This class's role is to draw on the canvas the grid and the people on it
+ * each person is represented by one dot with different colors according to their
+ * condition.
+ * 
+ * @author ssofok02 & asofro03
+ *
+ */
 public class DrawGrid {
 	static int I;
+	/**
+	 * Method to draw the grid representing the space where people will
+	 * move
+	 * 
+	 * @param N Size of the grid
+	 */
 	public static void DrawFrame(int N) {
 		int draw=N+1;
 		I=draw;
@@ -18,14 +31,23 @@ public class DrawGrid {
 		
 		
 	}
-	
+	/**
+	 * Method to draw a point representing a person
+	 * @param x	X coordinate of a person
+	 * @param y	Y coordinate of a person
+	 */
 	public static void drawPeople(int x,int y) {
 		StdDraw.setPenColor();
 		StdDraw.setPenRadius(1.1/I);
 		StdDraw.point(x+0.5, y+0.5);
 		
 	}
-	
+	/**
+	 * Method to draw a point representing a person using specific Color
+	 * @param x X coordinate of a person
+	 * @param y Y coordinate of a person
+	 * @param color Color of the point
+	 */
 	public static void drawPeople(int x,int y, Color color) {
 		StdDraw.setPenColor(color);
 		StdDraw.setPenRadius(1.1/I);
@@ -34,7 +56,11 @@ public class DrawGrid {
 	}
 	
 	
-	
+	/**
+	 * Method that removes a person from the current spot
+	 * @param x	X coordinate of a person
+	 * @param y Y coordinate of a person
+	 */
 	public static void removePeople(int x,int y) {
 		StdDraw.setPenColor(StdDraw.WHITE);
 		StdDraw.setPenRadius(1.2/I);
@@ -42,13 +68,11 @@ public class DrawGrid {
 		
 	}
 	
-	public static void drawCell(int x,int y,Color color) {
-		StdDraw.setPenColor(color);
-		StdDraw.filledRectangle(x+0.5, y+0.5, 0.5, 0.5);
-	}
 	
 
-	
+	/**
+	 * Method to make the animation slower
+	 */
 	public static void waitFrame() {
 		StdDraw.show(50);
 	}
