a20d79de090b10200bcbb72543e46b71112b2501
asofro03 <asofro03@ucy.ac.cy>
2020-04-16 17:44:16 +0300

    package info

--- src/sofokleous_sofroniou/hw5/package-info.java
+++ src/sofokleous_sofroniou/hw5/package-info.java
@@ -1,1 +1,18 @@
+/**
+ * In this package there are six classes. The first class, Point, is a
+ * representation of a position with the coordinates x,y. The second class,
+ * People, is a representation of a person. Third class, Cell, is a
+ * representation of a cell in a grid, a position of a human in a grid. Forth
+ * class, Grid, is a representation of our simulation with a grid and people.
+ * Fifth class, DrawGrid, is a class that draws our simulation. And the sixth
+ * class, Covid, is the driver class of our simulation. In this simulation, we
+ * create people and infect some of them with the virus. Then we monitor their
+ * health using a visual representation. At the end we show the end statistics
+ * of the simulation.
+ * 
+ * 
+ * @author ssofok02 and asofro03
+ * @since 14/04/20
+ * 
+ */
 package sofokleous_sofroniou.hw5;
